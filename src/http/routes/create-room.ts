ec05a19bf6066547cfecc2e6dda5bca5bf0b04b5
glatztp <gabrielfellipeglatz@gmail.com>
2025-07-09 21:10:27 -0300

    feat: finish project

--- src/http/routes/create-room.ts
+++ src/http/routes/create-room.ts
@@ -1,11 +1,11 @@
-import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
-import { z } from "zod/v4";
-import { db } from "../../db/connection.ts";
-import { schema } from "../../db/schema/index.ts";
+import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
+import { z } from 'zod/v4'
+import { db } from '../../db/connection.ts'
+import { schema } from '../../db/schema/index.ts'
 
-export const createRoomsRoute: FastifyPluginCallbackZod = (app) => {
+export const createRoomRoute: FastifyPluginCallbackZod = (app) => {
   app.post(
-    "/rooms",
+    '/rooms',
     {
       schema: {
         body: z.object({
@@ -14,23 +14,24 @@ export const createRoomsRoute: FastifyPluginCallbackZod = (app) => {
         }),
       },
     },
-    async (request, reply ) => {
-      const {name, description} = request.body
+    async (request, reply) => {
+      const { name, description } = request.body
 
-      const result =await db.insert(schema.rooms).values({
-        name,
-        description
-      }).returning()
+      const result = await db
+        .insert(schema.rooms)
+        .values({
+          name,
+          description,
+        })
+        .returning()
 
       const insertedRoom = result[0]
 
-      if (!insertedRoom){
-        throw new Error('Failed to reate new room')
+      if (!insertedRoom) {
+        throw new Error('Failed to create new room.')
       }
 
-      return reply.status(201).send({
-        roomId: insertedRoom.id
-      })
+      return reply.status(201).send({ roomId: insertedRoom.id })
     }
-  );
-};
+  )
+}
--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,37 @@
-import { fastify } from "fastify";
+import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
+import { fastify } from 'fastify'
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
-} from "fastify-type-provider-zod";
-import { env } from "./env.ts";
+} from 'fastify-type-provider-zod'
+import { env } from './env.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomQuestions } from './http/routes/get-room-questions.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
-import { fastifyCors } from "@fastify/cors";
-import { getRoomsRoute } from "./http/routes/get-rooms.ts";
-import { createRoomsRoute } from "./http/routes/create-room.ts";
-import { getRoomsQuestions } from "./http/routes/get-rooms-questions.ts";
-import { createQuestionRoute } from "./http/routes/create-question.ts";
-
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-  origin: "http://localhost:5173",
-});
+  origin: 'http://localhost:5173',
+})
 
-app.setSerializerCompiler(serializerCompiler);
-app.setValidatorCompiler(validatorCompiler);
+app.register(fastifyMultipart)
 
-app.get("/health", () => {
-  return "OK";
-});
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
 
-app.register(getRoomsRoute);
-app.register(createRoomsRoute);
-app.register(getRoomsQuestions);
-app.register(createQuestionRoute);
+app.get('/health', () => {
+  return 'OK'
+})
 
-app.listen({ port: env.PORT });
+app.register(getRoomsRoute)
+app.register(createRoomRoute)
+app.register(getRoomQuestions)
+app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
+
+app.listen({ port: env.PORT })
